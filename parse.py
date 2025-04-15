f1978ca5b5d343441147164a995c5254e7f2c86b
Pete <pjosev14@gmail.com>
2025-04-15 10:43:46 -0700

    change function

--- parse.py
+++ parse.py
@@ -1,23 +1,24 @@
 import csv
 
 
-def parse_file(file_path):
+def parse_file_to_dict(file_path):
     """
-    Parses a CSV file and returns a list of dictionaries.
-    Each dictionary represents a row in the CSV file.
+    Parses a CSV file into a dictionary and returns the key-value pairs.
     """
     with open(file_path, 'r') as file:
-        store = csv.DictReader(file)
-        for row in store:
-            print(row['color'], row['fruit'])
+        store = csv.reader(file)
+        dict_store = dict(store)
     
+        return dict_store
 
 if __name__ == "__main__":
     # Example usage
-    file_path = 'key-value.csv'
-    parse_file(file_path)
+    fruit_file = 'key-value.csv'
+    #parse_file_to_dict("key-value.csv")
+    parse_file_to_dict(fruit_file)
     # Output: color fruit
     #         red apple
     #         yellow banana
     #         green grape
     #         blue blueberry
+
